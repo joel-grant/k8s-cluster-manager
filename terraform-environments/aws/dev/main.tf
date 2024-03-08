e56766f297ec0539819744deac571ab8585c6cba
Joel Grant <69736499+joel-grant@users.noreply.github.com>
2024-03-07 22:42:09 -0700

    fix the availability zone typo (#22)

--- terraform-environments/aws/dev/main.tf
+++ terraform-environments/aws/dev/main.tf
@@ -26,7 +26,7 @@ module "vpc" {
   name = "rmcm-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2", "us-west-2d"]
+  azs             = ["us-west-2a", "us-west-2c", "us-west-2d"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
