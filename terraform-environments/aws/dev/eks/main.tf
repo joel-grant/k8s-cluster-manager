5ac347c4d6dad7726a0041f7107de6519043eb17
Joel Grant <joelmgrant@me.com>
2024-03-08 16:55:15 -0700

    run terraform fmt

--- terraform-environments/aws/dev/eks/main.tf
+++ terraform-environments/aws/dev/eks/main.tf
@@ -35,7 +35,7 @@ module "eks" {
   cluster_name    = "dev"
   cluster_version = "1.29"
 
-  cluster_endpoint_public_access  = true
+  cluster_endpoint_public_access = true
 
   cluster_addons = {
     coredns = {
