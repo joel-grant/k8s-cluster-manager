f2e84cb3d1d00cfd1ad120c25abf12dbc75bb979
Joel Grant <joelmgrant@me.com>
2024-03-09 14:46:48 -0700

    Add cloud config

--- terraform-environments/aws/dev/route_53/main.tf
+++ terraform-environments/aws/dev/route_53/main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = "us-west-2"
 }
 
+terraform {
+  cloud {
+    organization = "rocky-mountain-chile-man"
+
+    workspaces {
+      name = "kubernetes-ops-route53-dev"
+    }
+  }
+}
+
 module "zones" {
   source  = "terraform-aws-modules/route53/aws//modules/zones"
   version = "~> 2.0"
