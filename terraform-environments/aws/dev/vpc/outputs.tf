9d5743ea7d7d38d6bac63682fcf5f5cb8baf18a6
Joel Grant <69736499+joel-grant@users.noreply.github.com>
2024-03-09 09:23:43 -0700

    Test subnet fix (#27)
    * Test subnet fix
    
    * run terraform fmt
    
    * fix typo

--- terraform-environments/aws/dev/vpc/outputs.tf
+++ terraform-environments/aws/dev/vpc/outputs.tf
@@ -2,3 +2,7 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value       = module.vpc.vpc_id
 }
+output "private_subnets" {
+  description = "A list of private subnets"
+  value       = module.vpc.private_subnets
+}
