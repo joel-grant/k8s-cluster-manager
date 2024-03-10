91a56aa55fee61cc9dd0b366067de473df4f0fe9
Joel Grant <joelmgrant@me.com>
2024-03-10 09:26:21 -0600

    also try revertin to beta version

--- terraform-environments/aws/dev/autoscaler/main.tf
+++ terraform-environments/aws/dev/autoscaler/main.tf
@@ -38,7 +38,7 @@ provider "helm" {
     host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
     cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
     exec {
-      api_version = "client.authentication.k8s.io/v1"
+      api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", "dev"]
       command     = "aws"
     }
