9c7358fe0d82adcbf47ff92b717adb5d510aa232
Joel Grant <joelmgrant@me.com>
2024-03-09 20:39:42 -0700

    add the missing env var

--- terraform-environments/aws/dev/autoscaler/main.tf
+++ terraform-environments/aws/dev/autoscaler/main.tf
@@ -39,7 +39,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", "dev"]
       command     = "aws"
     }
   }
