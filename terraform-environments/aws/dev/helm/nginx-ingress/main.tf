a25607a83cb2df68de74afcd02fe71ebaed410ba
Joel Grant <69736499+joel-grant@users.noreply.github.com>
2024-03-15 20:36:25 -0600

    Add terraform workspace config (#52)
    * Add terraform workspace config
    
    * Update dependency versions
    
    * run terraform fmt

--- terraform-environments/aws/dev/helm/nginx-ingress/main.tf
+++ terraform-environments/aws/dev/helm/nginx-ingress/main.tf
@@ -1,3 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.38.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.11.0"
+    }
+    utils = {
+      source  = "cloudposse/utils"
+      version = ">= 0.17.0"
+    }
+  }
+
+  cloud {
+    organization = "rocky-mountain-chile-man"
+
+    workspaces {
+      name = "kubernetes-ops-dev-ingress-nginx"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
 module "eks-ingress-nginx" {
   source  = "lablabs/eks-ingress-nginx/aws"
   version = "1.2.0"
