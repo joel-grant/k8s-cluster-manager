b5364af4f786d516b883396b3c2e1f42f1fa459b
Joel Grant <joelmgrant@me.com>
2024-03-15 20:25:05 -0600

    Add terraform workspace config

--- terraform-environments/aws/dev/helm/nginx-ingress/main.tf
+++ terraform-environments/aws/dev/helm/nginx-ingress/main.tf
@@ -1,3 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.67.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.3.0"
+    }
+  }
+  cloud {
+    organization = "rocky-mountain-chile-man"
+
+    workspaces {
+      name = "kubernetes-ops-dev-ingress-nginx"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
 module "eks-ingress-nginx" {
   source  = "lablabs/eks-ingress-nginx/aws"
   version = "1.2.0"
